e215ff312dd56efa688855dddae5fa53f6867276
Akın Aksoy <akinaksoytr@gmail.com>
2023-01-20 15:01:14 +0300

    Project is created

---
+++ MovieApp2023/Scenes/DiscoverViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp2023
+//
+//  Created by Akın Aksoy on 20.01.2023.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
